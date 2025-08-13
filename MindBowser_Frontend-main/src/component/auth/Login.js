908ee70e5fd585c8cf57113343660b6c0e2c2cf7
thebhamaree <harshalbhamare2002@gmail.com>
2025-08-13 15:39:06 +0530

    Update Login.js

--- MindBowser_Frontend-main/src/component/auth/Login.js
+++ MindBowser_Frontend-main/src/component/auth/Login.js
@@ -20,7 +20,7 @@ function Login() {
 
   const handleLogin = async (e) => {
     e.preventDefault();
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://blogem-knty.onrender.com/api/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(formData),
